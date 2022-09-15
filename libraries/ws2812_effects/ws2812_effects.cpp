2370732acdeb3afc114449b6bd213f0ccaeacc2c
almiluk <almiluk@gmail.com>
2022-09-16 00:59:21 +0500

    add partial rainbow effect

--- libraries/ws2812_effects/ws2812_effects.cpp
+++ libraries/ws2812_effects/ws2812_effects.cpp
@@ -16,6 +16,7 @@ void WS2812Effecter::Show(){
 	case random_burst: random_burst_ef(); break;
 	case rainbow_vertical: rainbow_vertical_ef(); break;
 	case continuous_vertical_rainbow: continuous_vertical_rainbow_ef(); break;
+	case partial_rainbow: partial_rainbow_ef(); break;
 	
 	default:
 		SetEffect(off);
@@ -26,7 +27,7 @@ void WS2812Effecter::Show(){
 
 void WS2812Effecter::SetEffect(effect_t new_effect){
 	effect = new_effect;
-	sat = 255;
+	sat = val = 255;
 	switch (new_effect)
 	{
 	case off: SetColorHSV(0, 0, 0); break;
@@ -37,11 +38,12 @@ void WS2812Effecter::SetEffect(effect_t new_effect){
 	case random_burst: delay_time = 30; break;
 	case rainbow_vertical: step = 15; delay_time = 50; break;
 	case continuous_vertical_rainbow: step = 5; delay_time = 15; break;
+	case partial_rainbow: step = 1; delay_time = 50; hue = ihue = 255; break;
 	}
 }
 
 void WS2812Effecter::SetColorHSV(byte h, byte s, byte v){
-	hue = h;
+	ihue = h;
 	sat = s;
 	val = v;
 
@@ -50,66 +52,74 @@ void WS2812Effecter::SetColorHSV(byte h, byte s, byte v){
 
 void WS2812Effecter::SetColorRGB(byte r, byte g, byte b){
 	CHSV hsv = rgb2hsv_approximate(CRGB(r, g ,b));
-	hue = hsv.h;
+	ihue = hsv.h;
 	sat = hsv.s;
 }
 
 void WS2812Effecter::one_color_all(){
 	for (int i = 0 ; i < led_num; i++ ) {
-		leds[i] = CHSV(hue, sat, val);
+		leds[i] = CHSV(ihue, sat, val);
 	}
 	LEDS.show();
 }
 
 void WS2812Effecter::rainbow_fade_ef(){
-	hue = hue != 255 ? hue + 1 : 0;
+	ihue = ihue != 255 ? ihue + 1 : 0;
 	for (int i = 0 ; i < led_num; i++ ) {
-		leds[i] = CHSV(hue, sat, 255);
+		leds[i] = CHSV(ihue, sat, 255);
 	}
 	LEDS.show();
 	if (safeDelay(delay_time)) return;
 }
 
 void WS2812Effecter::rainbow_loop_ef(){
-	hue = hue + step <= 255 ? hue + step : 0;
+	ihue += step;
 	index = index < led_num - 1 ? index + 1 : 0;
-	leds[index] = CHSV(hue, sat, 255);
+	leds[index] = CHSV(ihue, sat, 255);
 	LEDS.show();
 	if (safeDelay(delay_time)) return;
 }
 
 void WS2812Effecter::new_rainbow_loop_ef(){
-	hue--;
-	fill_rainbow(leds, led_num, hue, step);
+	ihue--;
+	fill_rainbow(leds, led_num, ihue, step);
 	LEDS.show();
 	if (safeDelay(delay_time)) return;
 }
 
 void WS2812Effecter::random_burst_ef() {
 	index = random(0, led_num);
-	hue = random(0, 255);
-	leds[index] = CHSV(hue, sat, 255);
+	ihue = random(0, 255);
+	leds[index] = CHSV(ihue, sat, 255);
 	LEDS.show();
 	if (safeDelay(delay_time)) return;
 }
 
 void WS2812Effecter::rainbow_vertical_ef(){
-	hue = hue + step <= 255 ? hue + step : 0;
+	ihue += step;
 	index = index < led_num / 2 ? index + 1 : 0;
 
-	leds[index] = leds[horizontal_index(index)] = CHSV(hue, sat, 255);
+	leds[index] = leds[horizontal_index(index)] = CHSV(ihue, sat, 255);
 	LEDS.show();
 	if (safeDelay(delay_time)) return;
-	
 }
 
 void WS2812Effecter::continuous_vertical_rainbow_ef(){
-	hue--;
+	ihue--;
 	byte vert_led_cnt = led_num / 2 + led_num % 2;
-	fill_rainbow(leds, vert_led_cnt, hue, step);
+	fill_rainbow(leds, vert_led_cnt, ihue, step);
 	for(int i = 0; i < led_num - vert_led_cnt; i++)
 		leds[led_num - 1 - i] = leds[i];
 	LEDS.show();
+	if (safeDelay(delay_time)) return;
+}
+
+void WS2812Effecter::partial_rainbow_ef(){
+	ihue += step;
+	if(!is_near_hsv(ihue, hue, RAINBOW_PART_SIZE))
+		step = -step;
+	SetColorHSV(ihue, sat, val);
+	Serial.println(ihue);
 	if (safeDelay(delay_time)) return;
 }
 
@@ -117,7 +127,6 @@ byte WS2812Effecter::horizontal_index(byte i) {
 	byte bottom_ind = 0;
 	byte is_even = !(led_num % 2);
 	byte top_ind = led_num / 2 - is_even;
-
 
 	if (i == top_ind){
 		return top_ind + is_even;
@@ -130,5 +139,15 @@ bool WS2812Effecter::safeDelay(int del_time) {
 	while (millis() - thisTime <= del_time) {
 		delay(5);
 	}
+	return false;
+}
+
+bool WS2812Effecter::is_near_hsv(byte c1, byte c2, byte range){
+	if (abs(c1 - c2) < range)
+		return true;
+	if (c1 + range > 255 && c2 <= c1 + range - 255)
+		return true;
+	if (c1 - range < 0 && c2 >= 255 - (range - c1))
+		return true;
 	return false;
 }
--- libraries/ws2812_effects/ws2812_effects.h
+++ libraries/ws2812_effects/ws2812_effects.h
@@ -1,5 +1,8 @@
 #ifndef WS2812_EFFECTS_H
 #define WS2812_EFFECTS_H
+
+#define RAINBOW_PART_SIZE 30
+
 #include "FastLED.h"
  
 
@@ -7,7 +10,7 @@ class WS2812Effecter{
 public:
 	enum effect_t{
 		off, white, rainbow_fade, rainbow_loop, new_rainbow_loop, random_burst,
-		rainbow_vertical, continuous_vertical_rainbow
+		rainbow_vertical, continuous_vertical_rainbow, partial_rainbow
 	};
 
 	WS2812Effecter(CRGB* leds, byte led_num, effect_t effect = off, byte brightness = 0);
@@ -25,9 +28,10 @@ private:
 
 	byte index = 0;
 	byte hue = 0;
+	byte ihue = 0;
 	byte sat = 0;
-	byte val = 0;
-	byte step = 0;
+	byte val = 255;
+	int8_t step = 0;
 	byte delay_time = 0;
 
 	void one_color_all();
@@ -36,10 +40,12 @@ private:
 	void new_rainbow_loop_ef();
 	void random_burst_ef();
 	void rainbow_vertical_ef();
-	void continuous_vertical_rainbow_ef(); 
+	void continuous_vertical_rainbow_ef();
+	void partial_rainbow_ef();
 
 	byte horizontal_index(byte i);
 	bool safeDelay(int del_time);
+	bool is_near_hsv(byte c1, byte c2, byte range);
 };
 
 #endif
